bfe591c1aa2b94999b10b1e51f3d4703451b83e4
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-09-22 17:08:20 +0300

    Котики ищут дом

---
+++ KT2/cats.js
@@ -0,0 +1,103 @@
+export const cats = [
+  {
+    name: "Максим",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2021-09/167200DD-A44F-4845-8D4D-ACCFC180165A.jpeg",
+    age: 8,
+    rate: 8,
+    favourite: false,
+    description:
+      "Максик – шотландский вислоухий, у него остеохондродисплазия. Макс спокойный, очень ласковый и контактный. Болезнь не лечится и специального ухода не нужно.",
+    id: 1,
+  },
+  {
+    name: "Даниил",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/1_25.jpg",
+    age: 4,
+    rate: 10,
+    favourite: true,
+    description:
+      "Даня обладает мягким и добрым характером. Очень любит всевозможные лакомства и вкусно покушать. Не доверяет людям, потребуется время, чтобы стать ей другом. Приучен к лотку и когтеточке",
+    id: 4,
+  },
+  {
+    name: "София",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/064AEBCB-45EC-4CE7-AB13-C65F10F00B7B.jpeg",
+    age: 2,
+    rate: 8,
+    favourite: false,
+    description:
+      "Внимательная, активная и ласковая. Любит играть, катать мяч, и мурчать на пледе рядом с людьми! Прилично воспитана, приучена к лотку. Вакцинирована, имеет ветеринарный паспорт.",
+    id: 2,
+  },
+  {
+    name: "Константин",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/_DM34706.JPG",
+    age: 1,
+    rate: 8,
+    favourite: false,
+    description:
+      "Костя любит лесенки, канаты. Очень активный и дружелюбный кот. Костик полностью здоров, привит, кастрирован. Использует лоточек и очень аккуратен.",
+    id: 3,
+  },
+
+  {
+    name: "Фёдор",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/%D0%BB%D0%B5%D0%B2%D0%B83_%D0%B0%D0%BB%D0%B5%D0%BA%D1%81.jpg",
+    age: 1,
+    rate: 10,
+    favourite: true,
+    description:
+      "Чёрно-белый юный котофилософ очень любит размышлять и быть наедине. Пока что не доверяет людям, не агрессивный. Ладит с другими животными, приучен к лотку и когтеточке",
+    id: 5,
+  },
+  {
+    name: "Диана",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-02/B1444207-6EE3-4BA4-97F7-2F9666AE2F63.jpeg",
+    age: 1,
+    rate: 9,
+    favourite: true,
+    description:
+      "Дише около 1 года с небольшим, здорова, привита, стерилизована. Лоточек и когтеточку знает прекрасно. Очень общительная и нежная, хочет постоянного внимания.",
+    id: 7,
+  },
+
+  {
+    name: "Роман",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/IMG-20211223-WA0049.jpg",
+    age: 1,
+    rate: 10,
+    favourite: false,
+    description:
+      "Очаровательный активный кот Рома, находится в постоянном движении! Очаровательный и ласковый кот. Приучен к лотку, ладит с другими котами, привит.",
+    id: 8,
+  },
+  {
+    name: "Есения",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/4_30.jpg",
+    age: 6,
+    rate: 9,
+    favourite: true,
+    description:
+      "Прелестная Есения – трогательная, добродушная и очень-очень общительная девочка как никто другой нуждается в заботе и любви. Приучена к лотку и когтеточке",
+    id: 6,
+  },
+  {
+    name: "Эрик",
+    img_link:
+      "https://www.friendforpet.ru/api/sites/default/files/2022-01/%D1%81%D0%B2%D0%B5%D1%82%D0%BB%D1%8F%D1%87%D0%BE%D0%BA4_%D0%B0%D0%BB%D0%B5%D0%BA%D1%81.jpg",
+    age: 1,
+    rate: 9,
+    favourite: false,
+    description:
+      "Немного боязливый, но очень добрый и нежный кот Эрик. Приучен к лотку и когтеточке, ладит с детьми, привит. Станет вам хорошим другом",
+    id: 9,
+  },
+];
